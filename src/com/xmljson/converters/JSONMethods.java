bf3cc24fe5b4a98a05f595e6af90887adff5965b
suranya <suranyajayan@gmail.com>
2016-08-03 18:07:18 -0600

    XML conversion using javax.xml

--- src/com/xmljson/converters/JSONMethods.java
+++ src/com/xmljson/converters/JSONMethods.java
@@ -1,5 +1,6 @@
 package com.xmljson.converters;
 
+import java.io.File;
 import java.io.StringReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,6 +14,18 @@ import javax.json.JsonReader;
 import javax.json.JsonString;
 import javax.json.JsonStructure;
 import javax.json.JsonValue;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * Utilities Class contains methods for file read/write 
@@ -24,8 +37,11 @@ import javax.json.JsonValue;
 public class JSONMethods {
 	
 	private static final Logger LOGGER = Logger.getLogger (JSONMethods.class.getName ());
-	//String used for constructing xml in jsonValueToXml
-	private StringBuilder xmlString = new StringBuilder ();
+	
+	//XML Dom
+	private DocumentBuilderFactory docFactory;
+	private DocumentBuilder docBuilder; 
+	private Document doc;
 	
 	/**
 	 * xmlConverter reads json from an input string and converts it to
@@ -34,18 +50,38 @@ public class JSONMethods {
 	 * @param json String containing the json read from file
 	 * @return String containing the xml converted from json
 	 */
-	public String xmlConverter (String json) {
+	public void xmlConverter (String json, File xml) {
 		JsonStructure object = null;
 		try {
 			JsonReader jsonReader = Json.createReader(new StringReader (json));
 			object = jsonReader.read();
 		} catch (JsonException e) {
 			LOGGER.log (Level.SEVERE, "Exception while parsing JSON. Error in syntax or unexpected beginning tokens, not [ or { ");
-			return "";
+			LOGGER.log (Level.SEVERE, "Cannot convert file ");
+			return;
 		}
-		xmlString.setLength(0);
-		jsonValueToXml (object, null);
-		return xmlString.toString();
+		docFactory = DocumentBuilderFactory.newInstance();
+		try {
+			docBuilder = docFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			LOGGER.log (Level.SEVERE, "ParserConfigurationException while making XML Document builder");
+			LOGGER.log (Level.SEVERE, "Cannot convert file ");
+			return;
+		}
+		doc = docBuilder.newDocument();		
+		jsonValueToXml (object, null, null);		
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		DOMSource source = new DOMSource(doc);
+		StreamResult result = new StreamResult(xml);
+		try {
+			Transformer transformer = transformerFactory.newTransformer();
+			transformer.transform(source, result);		
+		} catch (TransformerException e) {
+			LOGGER.log (Level.SEVERE, "Exception while making file from XML");
+			LOGGER.log (Level.SEVERE, "Cannot write XML file ");
+			return;
+		}
+		
 	}
 	
 	/**
@@ -57,75 +93,98 @@ public class JSONMethods {
 	 * @param key Key corresponding to jsonValue
 	 * @return String containing the xml converted from json
 	 */
-	public void jsonValueToXml (JsonValue jsonValue, String key) {
+	private void jsonValueToXml (JsonValue jsonValue, String key, Element parent) {
 		switch (jsonValue.getValueType ()) {		
 			case OBJECT:
-				if (key != null) {
-					xmlString.append ("<object name=\"" + key + "\">");
+				Element obj = doc.createElement("object");
+				if (parent == null) {
+					doc.appendChild(obj);
 				} else {
-					xmlString.append ("<object>");
-			    }
+					parent.appendChild(obj);
+				}
+				if (key != null) {
+					Attr attr = doc.createAttribute("name");
+					attr.setValue(key);
+					obj.setAttributeNode(attr);
+				} 
 				JsonObject jsonObject = (JsonObject) jsonValue;
 		        for (String name : jsonObject.keySet ())
-		        	jsonValueToXml (jsonObject.get (name), name);
-		        xmlString.append ("</object>");
+		        	jsonValueToXml (jsonObject.get (name), name, obj);
 		        break;
 		        
 			case ARRAY:
+				Element arr = doc.createElement("array");
+				if (parent == null) {
+					doc.appendChild(arr);
+				} else {
+					parent.appendChild(arr);
+				}				
 				if (key != null) {
-					xmlString.append ("<array name=\"" + key + "\">");
-			     } else {
-			    	 xmlString.append ("<array>");
+					Attr attr = doc.createAttribute("name");
+					attr.setValue(key);
+					arr.setAttributeNode(attr);
 			     }
 		         JsonArray array = (JsonArray) jsonValue;
 		         for (JsonValue val : array)
-		        	 jsonValueToXml (val, null);
-		         xmlString.append ("</array>");
+		        	 jsonValueToXml (val, null, arr);
 		         break;
 		         
 			case STRING:
-		         JsonString string = (JsonString) jsonValue;
+				Element str = doc.createElement("string");
+				JsonString string = (JsonString) jsonValue;
+				str.appendChild(doc.createTextNode(string.getString ()));
+				parent.appendChild(str);		         
 		         if (key != null) {
-		        	 xmlString.append ("<string name=\"" + key + "\">" + string.getString () + "</string>");
-		         } else {
-		        	 xmlString.append("<string>" +string.getString () + "</string>");
-		         }
+		        	 Attr attr = doc.createAttribute("name");
+					 attr.setValue(key);
+					 str.setAttributeNode(attr); 
+		         } 
 		         break;
 		         
 			case NUMBER:
+				 Element number = doc.createElement("number");
 			     JsonNumber num = (JsonNumber) jsonValue;
+			     number.appendChild(doc.createTextNode(num.toString ()));
+				 parent.appendChild(number);
 			     if (key != null) {
-			    	 xmlString.append ("<number name=\"" + key + "\">" + num.toString () + "</number>");
-		         } else {
-		        	 xmlString.append ("<number>" + num.toString () + "</number>");
-		         }
+			    	 Attr attr = doc.createAttribute("name");
+					 attr.setValue(key);
+					 number.setAttributeNode(attr); 
+		         } 
 		         break;
 		         
 			case TRUE:
+				Element bool = doc.createElement("boolean");
+				bool.appendChild(doc.createTextNode("true"));
+				parent.appendChild(bool);
 				if (key != null) {
-					xmlString.append ("<boolean name=\"" + key + "\">true</boolean>");
-		         } else {
-		        	xmlString.append ("<boolean>true</boolean>");
-		         }
+					Attr attr = doc.createAttribute("name");
+					 attr.setValue(key);
+					 bool.setAttributeNode(attr); 
+		         } 
 				break;
 	    	  
 			case FALSE:
+				Element boolFalse = doc.createElement("boolean");
+				boolFalse.appendChild(doc.createTextNode("false"));
+				parent.appendChild(boolFalse);
 				if (key != null) {
-					xmlString.append ("<boolean name=\"" + key + "\">false</boolean>");
-		        } else {
-		        	xmlString.append ("<boolean>false</boolean>");
-		        }
+					Attr attr = doc.createAttribute("name");
+					 attr.setValue(key);
+					 boolFalse.setAttributeNode(attr); 
+		        } 
 				break;
 				
 			case NULL:
+				Element nullValue = doc.createElement("null");
+				parent.appendChild(nullValue);
 				if (key != null) {
-					xmlString.append ("<null name=\"" + key + "\"/>");
-		        } else {
-		        	xmlString.append ("<null/>");
-		        }
+					Attr attr = doc.createAttribute("name");
+					 attr.setValue(key);
+					 nullValue.setAttributeNode(attr); 
+		        } 
 				break;
 		}
-		//return xmlString.toString();
 	}
 
 }
